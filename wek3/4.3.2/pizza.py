b5c213775b6606e62d6e0c14300c96e7b7c6bf27
q1wf3 <151677823+q1wf3@users.noreply.github.com>
2025-03-05 15:18:37 +0300

    pizza.py

--- wek3/4.3.2/pizza.py
+++ wek3/4.3.2/pizza.py
@@ -1,21 +1,11 @@
-class Order:
-    def __init__(self):
-        self.items = []
+class Pizza:
+    def __init__(self, name, price, ingredients):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
 
-    def add_pizza(self, pizza):
-        self.items.append(pizza)
-
-    def calculate_total(self):
-        total = 0
-        for pizza in self.items:
-            total += pizza.price
-        return total
+    def prepare(self):
+        print(f"Готовим пиццу {self.name}...")
 
     def __str__(self):
-        if not self.items:
-            return "В заказе нет пицц."
-        order_str = "Заказ:\n"
-        for pizza in self.items:
-            order_str += f"  - {pizza}\n"
-        order_str += f"Итого: {self.calculate_total()} руб."
-        return order_str
+        return f"{self.name} ({self.price} руб.) - {', '.join(self.ingredients)}"
